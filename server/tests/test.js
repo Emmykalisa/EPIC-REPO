9ea935814978dd0af5566cd90ca76e0d5e8c6807
Emmykalisa <emmykalisa8@gmail.com>
2019-03-11 11:40:51 +0200

    Testing

--- server/tests/test.js
+++ server/tests/test.js
@@ -7,15 +7,38 @@ chai.should();
 /*
   * Test the /GET route
   */
- describe('/Get messages', () => {
+ describe('/Get All messages', () => {
     it('it should GET all emails/messages', (done) => {
       chai.request(server)
           .get('/api/v1/messages')
           .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.a('array');
-                // res.body.length.should.be.eql(0);
             done();
           });
     });
+});
+
+describe('/Get Unread messages', () => {
+  it('it should GET all Unread messages', (done) => {
+    chai.request(server)
+        .get('/api/v1/messages/unread')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+          done();
+        });
+  });
+});
+
+describe('/Get Sent messages', () => {
+  it('it should GET all sent messages', (done) => {
+    chai.request(server)
+        .get('/api/v1/messages/sent')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('array');
+          done();
+        });
+  });
 });
